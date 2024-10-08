431c44502847118526e558264ec28aa0c1d5952f
ugurarslan7 <ugurarslan8@hotmail.com>
2024-10-08 23:52:06 +0300

    finish project

---
+++ NLayerApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLayerApp.Core.Services;
+
+namespace NLayerApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var customResponse = await _productService.GetProductsWithCategoryAsync();
+            return View(customResponse.Data);
+        }
+    }
+}
