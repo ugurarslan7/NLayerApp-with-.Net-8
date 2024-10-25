3b2043bfa49351aedb5fc9d7d4e9f9f7ce6ee2a0
ugurarslan7 <ugurarslan8@hotmail.com>
2024-10-25 14:58:39 +0300

    category api changed

--- NLayerApp.API/Controllers/CategoriesController.cs
+++ NLayerApp.API/Controllers/CategoriesController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NLayerApp.Core.DTOs;
 using NLayerApp.Core.Model;
 using NLayerApp.Core.Services;
 
@@ -8,16 +10,57 @@ namespace NLayerApp.API.Controllers
     public class CategoriesController : CustomBaseController
     {
         private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
-        public CategoriesController(ICategoryService categoryService)
+        public CategoriesController(ICategoryService categoryService, IMapper mapper)
         {
             _categoryService = categoryService;
+            _mapper = mapper;
         }
 
         [HttpGet("[action]/{categoryId}")]
         public async Task<IActionResult> GetSingleCategoryByIdWithProductAsync(int categoryId)
         {
             return CreateActionResult(await _categoryService.GetSingleCategoryByIdWithProductAsync(categoryId));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CategoryDto categoryDto)
+        {
+            var category = await _categoryService.AddAsync(_mapper.Map<Category>(categoryDto));
+
+            var categoryDtos = _mapper.Map<CategoryDto>(category);
+
+            return CreateActionResult(CustomResponseDto<CategoryDto>.Success(201, categoryDtos));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(CategoryDto categoryDto)
+        {
+            await _categoryService.UpdateAsync(_mapper.Map<Category>(categoryDto));
+
+            return CreateActionResult(CustomResponseDto<CategoryDto>.Success(204));
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+
+            await _categoryService.RemoveAsync(category);
+
+            return CreateActionResult(CustomResponseDto<CategoryDto>.Success(204));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var category = await _categoryService.GetAllAsync();
+
+            var categoryDtos = _mapper.Map<List<CategoryDto>>(category.ToList());
+
+            return CreateActionResult(CustomResponseDto<List<CategoryDto>>.Success(200, categoryDtos));
         }
     }
 }
